3436e0148eaa6d494a4b845bbe5a86867682de98
JiriKrizek <jkrizek@gmail.com>
2013-01-17 12:26:03 +0100

    Added notification using notify-send

--- setvol.sh
+++ setvol.sh
@@ -2,3 +2,5 @@
 #
 #echo "Setting volume to $1"
 amixer set 'Master',0 "$1"% "$1"% >/dev/null 2>&1
+NOTIFY_UID=1000
+DISPLAY=:0 sudo -u \#$NOTIFY_UID notify-send --hint=int:transient:1 --expire-time=1 "Sound set to $1%" --urgency=low
